72a062e9369ebe77c381dd7155120c5533380f31
Anton <scientistsmartbee@gmail.com>
2025-11-14 21:09:12 +0200

    Fix route

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,14 +1,11 @@
 import { NextResponse } from "next/server";
 
-// временное хранилище (память)
-// позже заменим его на Kafka producer
 const events: any[] = [];
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
 
-    // простая валидация
     if (!body.type || !body.timestamp) {
       return NextResponse.json(
         { error: "Invalid event format" },
@@ -16,7 +13,6 @@ export async function POST(req: Request) {
       );
     }
 
-    // сохраняем событие в память
     events.push(body);
 
     return NextResponse.json({ ok: true });
